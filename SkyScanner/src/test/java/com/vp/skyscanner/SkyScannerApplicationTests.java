fb28d7fcc84ee333ab30c3e250dda43bba3e242d
Viktor Prusak <viktor.prusak@gmail.com>
2023-07-12 20:30:58 +0200

    created new project

---
+++ SkyScanner/src/test/java/com/vp/skyscanner/SkyScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vp.skyscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
