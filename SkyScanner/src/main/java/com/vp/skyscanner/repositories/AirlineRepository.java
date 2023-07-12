903eb2ea1d677eeb8b012807b65ad363b115a91f
Viktor Prusak <viktor.prusak@gmail.com>
2023-07-12 20:22:07 +0200

    feat(): added new repository for airline

---
+++ SkyScanner/src/main/java/com/vp/skyscanner/repositories/AirlineRepository.java
@@ -0,0 +1,12 @@
+package com.vp.skyscanner.repositories;
+
+import com.vp.skyscanner.models.Airline;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
+
+  Optional<Airline> findByName(String name);
+}
