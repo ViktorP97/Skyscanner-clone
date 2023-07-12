4871d0efb1500b169b17867aba010fa641867ad8
Viktor Prusak <viktor.prusak@gmail.com>
2023-07-12 20:26:51 +0200

    feat(): added new class for flight service

---
+++ SkyScanner/src/main/java/com/vp/skyscanner/services/FlightService.java
@@ -0,0 +1,16 @@
+package com.vp.skyscanner.services;
+
+import com.vp.skyscanner.dtos.FlightDto;
+import com.vp.skyscanner.dtos.RequestDto;
+import com.vp.skyscanner.models.Flight;
+import java.util.List;
+
+public interface FlightService {
+
+  FlightDto convertFlightToDto(Flight flight);
+
+  List<FlightDto> getAllFlights();
+
+  List<FlightDto> getAllFlightsBasedOnFromAndTo(RequestDto requestDto);
+
+}
