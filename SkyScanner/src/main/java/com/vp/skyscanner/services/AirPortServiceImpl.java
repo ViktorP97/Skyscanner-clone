fb9759514b824cc8bb62b48b1c57bc5547d79e21
Viktor Prusak <viktor.prusak@gmail.com>
2023-07-27 22:29:54 +0200

    fix(): fixed method for using tickets, when ticket is once used it cant be used again

--- SkyScanner/src/main/java/com/vp/skyscanner/services/AirPortServiceImpl.java
+++ SkyScanner/src/main/java/com/vp/skyscanner/services/AirPortServiceImpl.java
@@ -2,6 +2,7 @@ package com.vp.skyscanner.services;
 
 import com.vp.skyscanner.models.Ticket;
 import com.vp.skyscanner.models.UserEntity;
+import com.vp.skyscanner.repositories.BaggageRepository;
 import com.vp.skyscanner.repositories.TicketRepository;
 import com.vp.skyscanner.repositories.UserRepository;
 import java.util.Optional;
@@ -13,9 +14,12 @@ public class AirPortServiceImpl implements AirportService {
   private final UserRepository userRepository;
   private final TicketRepository ticketRepository;
 
-  public AirPortServiceImpl(UserRepository userRepository, TicketRepository ticketRepository) {
+  private final BaggageRepository baggageRepository;
+  public AirPortServiceImpl(UserRepository userRepository, TicketRepository ticketRepository,
+      BaggageRepository baggageRepository) {
     this.userRepository = userRepository;
     this.ticketRepository = ticketRepository;
+    this.baggageRepository = baggageRepository;
   }
 
   @Override
@@ -25,10 +29,12 @@ public class AirPortServiceImpl implements AirportService {
       return "Ticket not found";
     }
     Ticket ticket = ticketOptional.get();
+    if (ticket.isUsed()) {
+      return "Ticket already used";
+    }
     ticket.setUsed(true);
     user.getTickets().remove(ticket);
     userRepository.save(user);
-
     return "Enjoy you flight";
   }
 }
