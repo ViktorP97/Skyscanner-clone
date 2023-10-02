3dc18b8d290c1154ef35846e65d2a7a348803ce8
Viktor Prusak <viktor.prusak@gmail.com>
2023-10-02 20:02:26 +0200

    fix(): using registration service

--- SkyScanner/src/main/java/com/vp/skyscanner/controllers/AuthController.java
+++ SkyScanner/src/main/java/com/vp/skyscanner/controllers/AuthController.java
@@ -5,13 +5,19 @@ import com.vp.skyscanner.dtos.LoginDto;
 import com.vp.skyscanner.dtos.PasswordDto;
 import com.vp.skyscanner.dtos.RegisterDto;
 import com.vp.skyscanner.models.UserEntity;
+import com.vp.skyscanner.services.RegistrationService;
 import com.vp.skyscanner.services.UserService;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,20 +26,39 @@ public class AuthController {
 
   private final UserService userService;
 
+  private final RegistrationService registrationService;
+
   @Autowired
-  public AuthController(UserService userService) {
+  public AuthController(UserService userService, RegistrationService registrationService) {
     this.userService = userService;
+    this.registrationService = registrationService;
   }
 
   @PostMapping("/register")
   public ResponseEntity<String> register(@RequestBody RegisterDto registerDto) {
-    return ResponseEntity.ok(userService.register(registerDto));
+    return ResponseEntity.ok(registrationService.register(registerDto));
+  }
+
+  @GetMapping("/confirm")
+  public ResponseEntity<String> confirmRegistration(@RequestParam String token) {
+    UserEntity user = registrationService.confirmRegistration(token);
+
+    return new ResponseEntity<>(
+        "Your account has been successfully verified " + user.getUsername()
+            + ". You can now log in.",
+        HttpStatus.OK);
   }
 
   @PostMapping("/login")
   public ResponseEntity login(@RequestBody LoginDto loginDto) {
-    AuthDto authDto = userService.login(loginDto);
-    return ResponseEntity.ok(authDto);
+    try {
+      AuthDto authDto = registrationService.login(loginDto);
+      return ResponseEntity.ok(authDto);
+    } catch (BadCredentialsException e) {
+      // Handle the BadCredentialsException here and return a customized response
+      String errorMessage = e.getMessage();
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(errorMessage);
+    }
   }
 
   @PostMapping("/change")
