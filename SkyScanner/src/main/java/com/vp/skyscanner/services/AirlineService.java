f5b3a175ab50e2b9173b77e208f24542749ade03
Viktor Prusak <viktor.prusak@gmail.com>
2023-10-13 19:32:42 +0200

    fix(): removed unused imports

--- SkyScanner/src/main/java/com/vp/skyscanner/services/AirlineService.java
+++ SkyScanner/src/main/java/com/vp/skyscanner/services/AirlineService.java
@@ -1,9 +1,12 @@
 package com.vp.skyscanner.services;
 
 import com.vp.skyscanner.dtos.AirlineDto;
+import com.vp.skyscanner.dtos.RatingDto;
 import java.util.List;
 
 public interface AirlineService {
 
   List<AirlineDto> showAllAirlines();
+
+  String addRating(Long airlineId, RatingDto ratingDto);
 }
