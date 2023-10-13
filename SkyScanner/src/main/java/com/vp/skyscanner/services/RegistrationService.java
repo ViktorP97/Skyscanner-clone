e53044becdb662c7cf0e8e39f3ffcdddd97729db
Viktor Prusak <viktor.prusak@gmail.com>
2023-10-13 19:35:47 +0200

    feat(): new service for registration

---
+++ SkyScanner/src/main/java/com/vp/skyscanner/services/RegistrationService.java
@@ -0,0 +1,17 @@
+package com.vp.skyscanner.services;
+
+import com.vp.skyscanner.dtos.AuthDto;
+import com.vp.skyscanner.dtos.LoginDto;
+import com.vp.skyscanner.dtos.RegisterDto;
+import com.vp.skyscanner.dtos.RegisterResponseDto;
+import com.vp.skyscanner.models.UserEntity;
+
+public interface RegistrationService {
+
+  String register(RegisterDto registerDto);
+
+  UserEntity confirmRegistration(String token);
+
+  AuthDto login(LoginDto loginDto);
+}
+
